f5328ddfa3197f55a4c4c4828af0a3d32dff46d0
Joel Maldonado <domtheworldllc@gmail.com>
2026-03-05 11:32:47 -0500

    fix: render Lucide icons in template cards

--- app_js_blob.py
+++ app_js_blob.py
@@ -1,6 +1,1 @@
-import base64, zlib
-
-_COMPRESSED = "eNrsvel620iSKPrfT5GldhfJNklRmy1LZftQ...SBw=="
-
-def get_app_js():
-    return zlib.decompress(base64.b64decode(_COMPRESSED)).decode("utf-8")
+PLACEHOLDER_BLOB
